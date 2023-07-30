af3830e000fa293e9a4cf6d020b9528401401327
Gábor Csárdi <csardi.gabor@gmail.com>
2023-07-30 21:38:13 +0200

    Add support up to R 0.62.4

--- r-builds/build.sh
+++ r-builds/build.sh
@@ -15,11 +15,20 @@ else
 fi
 
 CRAN=http://cran.r-project.org
-BUILD_DIR==/opt/R/${R_VERSION}
 
 prepare() {
-    mkdir -p /opt/R
+    if [ -z "$1" ]; then
+	echo "Usage: prepare <r-version>"
+	return 100
+    fi
+    local rver=$1;
+    if dpkg --compare-versions "${rver}" lt 0.62; then
+        mkdir -p /opt/R
+    fi
 }
+
+# Not all versions need all these, but we might as well
+# install everything, it does not hurt
 
 install_requirements() {
     apt-get update -y
@@ -29,6 +38,8 @@ install_requirements() {
 	    patch            \
 	    gcc              \
 	    g77              \
+	    gcc-2.95         \
+	    g77-2.95         \
 	    f2c              \
 	    libc6-dev        \
 	    make             \
@@ -46,12 +57,11 @@ install_requirements() {
 fetch_r_source() {
     if [ -z "$1" ]; then
 	echo "Usage: fetch_r_source <r-version>"
-	return
+	return 100
     fi
     local rver="$1"
-    local build_dir="/opt/R/${rver}"
     echo "Downloading R-${rver}"
-    if [ "$rver" = "0.60" -o "$rver" = "0.61" ]; then
+    if [ "$rver" = "0.60" -o "$rver" = "0.61" -o "$rver" = "0.62" ]; then
 	local url="${CRAN}/src/base/R-0/R-${rver}.0.tgz"
     else
 	local url="${CRAN}/src/base/R-0/R-${rver}.tgz"
@@ -59,22 +69,30 @@ fetch_r_source() {
     wget "$url" -O R.tgz
     tar xzf R.tgz
     rm R.tgz
-    mv R-${rver} ${build_dir}
+    if dpkg --compare-versions "${rver}" lt 0.62; then
+        local build_dir="/opt/R/${rver}"
+        mv R-${rver} ${build_dir}
+    fi
 }
 
 patch_r_source() {
     if [ -z "$1" ]; then
 	echo "Usage: patch_r_source <r-version>"
-	return
+	return 100
     fi
-    local build_dir="/opt/R/${1}"
+    local rver="$1"
+    if dpkg --compare-versions "${rver}" lt 0.62; then
+        local build_dir="/opt/R/${rver}"
+    else
+        local build_dir="R-${rver}"
+    fi
     (
-	if [ -f patch/R-${1}.patch ]; then
+	if [ -f patch/R-${rver}.patch ]; then
 	    echo "Applying patch patch/R-${1}.patch"
-	    cp patch/R-${1}.patch ${build_dir}
+	    cp patch/R-${rver}.patch ${build_dir}
 	    cd ${build_dir}
-	    patch -p1 < R-${1}.patch
-	    rm R-${1}.patch
+	    patch -p1 < R-${rver}.patch
+	    rm R-${rver}.patch
 	fi
     )
 }
@@ -82,23 +100,43 @@ patch_r_source() {
 configure_r() {
     if [ -z "$1" ]; then
 	echo "Usage: configure_r <r-version>"
-	return
+	return 100
     fi
-    local build_dir="/opt/R/${1}"
+    local rver="$1"
+    if dpkg --compare-versions "${rver}" lt 0.62; then
+        local build_dir="/opt/R/${rver}"
+    else
+        local build_dir="R-${rver}"
+    fi
     (
 	cd ${build_dir}
-	./configure                         \
-	    --x-includes=/usr/X11R6/include \
-	    --x-libraries=/usr/X11R6/lib
+        if dpkg --compare-versions "${rver}" lt 0.62; then
+	    ./configure                         \
+	        --x-includes=/usr/X11R6/include \
+	        --x-libraries=/usr/X11R6/lib
+        elif dpkg --compare-versions "${rver}" lt 0.62.3; then
+	    ./configure                         \
+                --prefix="/opt/R/${rver}"       \
+	        --x-includes=/usr/X11R6/include \
+	        --x-libraries=/usr/X11R6/lib
+        else
+            ./configure                         \
+                --prefix="/opt/R/${rver}"
+        fi
     )
 }
 
 build_r() {
     if [ -z "$1" ]; then
 	echo "Usage: build_r <r-version>"
-	return
+	return 100
     fi
-    local build_dir="/opt/R/${1}"
+    local rver="$1"
+    if dpkg --compare-versions "${rver}" lt 0.62; then
+        local build_dir="/opt/R/${rver}"
+    else
+        local build_dir="R-${rver}"
+    fi
     (
 	cd ${build_dir}
 	make
@@ -109,29 +147,19 @@ build_r() {
 	grep '^install-help:' Makefile && make install-help
 	grep '^help:' Makefile && make help
 	grep '^docs:' Makefile && make docs
+        if dpkg --compare-versions "$rver" lt 0.62; then
+	    find ${build_dir} -name "*.o" | xargs rm
+        fi
     )
 }
 
-install_r() {
+package_r_in_place() {
     if [ -z "$1" ]; then
-	echo "Usage: install_r <r-version>"
-	return
-    fi
-    local build_dir="/opt/R/${1}"
-    (
-	cd ${build_dir}
-	# we could probably clean up more...
-	find ${build_dir} -name "*.o" | xargs rm
-    )
-}
-
-package_r() {
-    if [ -z "$1" ]; then
-	echo "Usage: patch_r_source <r-version>"
-	return
+	echo "Usage: package_r_in_place <r-version>"
+	return 100
     fi
     local rver="$1"
-    local build_dir="/opt/R/${1}"
+    local build_dir="/opt/R/${rver}"
     rm -rf /tmp/${rver}
     mv ${build_dir} /tmp/
     rm -rf ${build_dir}
@@ -152,21 +180,57 @@ Depends: less, xlibs, libreadline5"
 		 cp -r /tmp/${rver} ${build_dir}
 }
 
+package_r() {
+    if [ -z "$1" ]; then
+	echo "Usage: package_r <r-version>"
+	return 100
+    fi
+    local rver="$1"
+    local build_dir="R-${rver}"
+    echo "GNU R statistical computation and graphics system" > \
+	 description-pak
+    # Need a hack to add dependencies
+    export MAINTAINER="csardi.gabor@gmail.com
+Depends: less, xlibs, libreadline5"
+    (
+        cd ${build_dir}
+        checkinstall -D -y                               \
+		     --arch i386                         \
+		     --pkgname r-${rver}                 \
+		     --pkgversion 1                      \
+		     --pkgsource "https://r-project.org" \
+		     --pkglicense "GPL 2.0"              \
+		     --strip=yes --stripso=yes           \
+		     --spec r.spec                       \
+		     --delspec=no                        \
+		     make install
+        cp r-*.deb ..
+    )
+}
+
 build() {
-    prepare
-    fetch_r_source ${R_VERSION}
+    if [ -z "$1" ]; then
+	echo "Usage: build <r-version>"
+	return 100
+    fi
+    local rver="$1"
+    prepare ${rver}
+    fetch_r_source ${rver}
     install_requirements
-    patch_r_source ${R_VERSION}
-    configure_r ${R_VERSION}
-    build_r ${R_VERSION}
-    install_r ${R_VERSION}
-    package_r ${R_VERSION}
+    patch_r_source ${rver}
+    configure_r ${rver}
+    build_r ${rver}
+    if dpkg --compare-versions "${rver}" lt 0.62; then
+        package_r_in_place ${rver}
+    else
+        package_r ${rver}
+    fi
 }
 
 if [ "$sourced" = "0" ]; then
     # quit on error
     set -e
-    build
+    build ${R_VERSION}
 else
     # verbose if interactive
     set -x
