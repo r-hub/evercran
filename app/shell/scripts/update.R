2ecfd574ca1b52aaef026532311458166b3d0d39
Gábor Csárdi <csardi.gabor@gmail.com>
2023-10-27 18:41:14 +0200

    App: daily /packages update, always update
    There are DB updates every 15 minutes, so
    we need (try) to updates the PACKAGES files
    once a day.

--- app/shell/scripts/update.R
+++ app/shell/scripts/update.R
@@ -1,10 +1,8 @@
 source("lib.R")
 
 update_main <- function() {
-  response <- update_db()
-  if (length(response$results) > 0) {
-    init_packages(force = TRUE)
-  }
+  update_db()
+  init_packages(force = TRUE)
 }
 
 if (is.null(sys.calls())) {
