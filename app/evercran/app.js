82d3e225699752a8ba4c27b822e935d5aac8a19d
Gábor Csárdi <csardi.gabor@gmail.com>
2024-04-20 13:12:19 +0200

    Use node 20, log requests

--- app/evercran/app.js
+++ app/evercran/app.js
@@ -1,10 +1,12 @@
 import express from 'express';
+import logger from 'morgan';
 
 import metaRouter from './routes/meta.js';
 import fileRouter from './routes/file.js';
 
 export var app = express();
 
+app.use(logger('combined'));
 app.use(express.static("/packages"));
 
 app.use('/', metaRouter);
